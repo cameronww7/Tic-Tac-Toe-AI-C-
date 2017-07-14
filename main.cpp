f761b791c50300966251fc2784bba7ef09d65328
cam.w.w <cameronww7@users.noreply.github.com>
2017-07-13 17:54:57 -0700

    Adding Main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 13, 2017
+ *      Author: lpxpscww
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	std::cout << "Tic Tac Toe C++";
+
+	return 0;
+}
+
+
+
